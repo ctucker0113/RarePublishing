1246cf5b2c8bf7906a98ec31b8cb03b17075db6b
ctucker0113 <134985404+ctucker0113@users.noreply.github.com>
2024-02-05 21:13:02 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+// Start Coding Here!
+
+Console.WriteLine("Bonjour!");
+app.Run();
