f2d9c67db591e133af2f95f94e04453c882d9ad3
cnfairall <101303134+cnfairall@users.noreply.github.com>
2024-02-06 20:29:05 -0600

    category api calls

--- Api/Categories.cs
+++ Api/Categories.cs
@@ -1,6 +1,24 @@
-﻿namespace RarePublishing.Api
+﻿using RarePublishing.Models;
+using RarePublishing.Data;
+
+app.MapGet("/api/categories", () =>
 {
-    public class Categories
+    return Results.Ok(categories);
+});
+
+app.MapGet("/api/categories/{id}", (int id) =>
+{
+    Category category = categories.FirstOrDefault(c => c.Id == id);
+    if (category== null)
     {
+        return Results.NotFound();
     }
-}
+    return Results.Ok(category);
+});
+
+app.MapPost("/api/categories", (Category category) =>
+{
+    category.Id = categories.Max(c => c.Id) + 1;
+    categories.Add(category);
+    return category;
+});
